e5dae6510aa2d71036b68eb9a50945c6363e4dcd
Joel Jacob <80267776+Joeljacob04@users.noreply.github.com>
2021-04-19 07:30:14 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,23 @@
+class Roof {
+    constructor(x, y, width, height) {
+      var options = {
+          'isStatic':true,
+          'restitution':0,
+          'friction':0,
+          'density':1
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+        push();
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill("Yellow");
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+    }
+  };
